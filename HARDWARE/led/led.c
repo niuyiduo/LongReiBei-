d77097a9052aa750ed2855c7bebae7c7f44fe4c4
niuyiduo <165349057+niuyiduo@users.noreply.github.com>
2024-06-04 17:34:39 +0800

    Update led.c

--- HARDWARE/led/led.c
+++ HARDWARE/led/led.c
@@ -12,19 +12,19 @@
 #include "stm32f4xx_usart.h"
 #include "max30205.h"
 
-//½«LEDµÄÕı¼«£¨³¤Òı½Å£©Á¬½Óµ½STM32F407ZGT6µÄPB12Òı½ÅÉÏ£»½«LED¸º¼«£¨¶ÌÒı½Å£©Á¬½ÓÔÚGNDÉÏ¡£
+//å°†LEDçš„æ­£æï¼ˆé•¿å¼•è„šï¼‰è¿æ¥åˆ°STM32F407ZGT6çš„PB12å¼•è„šä¸Šï¼›å°†LEDè´Ÿæï¼ˆçŸ­å¼•è„šï¼‰è¿æ¥åœ¨GNDä¸Šã€‚
 
 
 #define MAX_TEMP_THRESHOLD 20.0f // Define the temperature threshold
 
-// LED?????
+
 void LED_Init(void) {
     GPIO_InitTypeDef GPIO_InitStruct;
 
-    // ??GPIOB??
+    
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 
-    // ??LED???????
+    
     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12; 
     GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
@@ -33,13 +33,13 @@ void LED_Init(void) {
     GPIO_Init(GPIOB, &GPIO_InitStruct); 
 }
 
-// ??LED??
+
 void LED_On(void) {
-    GPIO_SetBits(GPIOB, GPIO_Pin_12); // ??LED,?
+    GPIO_SetBits(GPIOB, GPIO_Pin_12); 
 }
 
-// ??LED??
+
 void LED_Off(void) {
-    GPIO_ResetBits(GPIOB, GPIO_Pin_12); // ??LED,
+    GPIO_ResetBits(GPIOB, GPIO_Pin_12); 
 }
 
