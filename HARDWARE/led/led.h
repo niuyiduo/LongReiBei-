6ae038bfcaf8b1b3697f0a91e0a340bdceb131ae
niuyiduo <165349057+niuyiduo@users.noreply.github.com>
2024-06-04 17:35:10 +0800

    Update led.h

--- HARDWARE/led/led.h
+++ HARDWARE/led/led.h
@@ -5,19 +5,16 @@
 
 #include "stm32f4xx.h"
 
-// ??LED?????
+
 #define LED_PIN GPIO_PIN_10
 #define LED_PORT GPIOF
 
-// ???LED??
+
 void LED_Init(void);
 
-// ??LED
+
 void LED_On(void);
 
-// ??LED
 void LED_Off(void);
-
-void CheckTemperatureAndControlLED(void);
 
 #endif /* LED_H */
